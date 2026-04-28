51f86630f7d86f8ac87886becc463af750c99844
cokes238 <cokes238@gmail.com>
2026-04-28 15:38:25 +0300

    Step 4: HeroesStore and HeroesController

--- HeroesApi/Program.cs
+++ HeroesApi/Program.cs
@@ -3,27 +3,28 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment()) {
-app. UseSwagger();
-app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-app. UseAuthorization();
-app.MapControllers();
-
-app.Run();
 builder.Services.AddControllers()
     .AddJsonOptions(options => {
-        options.JsonSerializerOptions.PropertyNamingPolicy =
+        options.JsonSerializerOptions.PropertyNamingPolicy = 
             JsonNamingPolicy.CamelCase;
         options.JsonSerializerOptions.Converters
             .Add(new JsonStringEnumConverter());
         options.JsonSerializerOptions.WriteIndented = true;
     });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment()) 
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
